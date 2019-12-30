1dee202a54260a36b1c5dd8b68a6722ba4e76103
Greg Mefford <greg@gregmefford.com>
2019-12-30 15:55:00 -0500

    Add .envrc files for easier local dev

--- phoenix_backend/config/dev.exs
+++ phoenix_backend/config/dev.exs
@@ -16,7 +16,7 @@ config :phoenix_backend, PhoenixBackend.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :phoenix_backend, PhoenixBackendWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4001],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
