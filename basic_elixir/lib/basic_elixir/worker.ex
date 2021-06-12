f36d76a06099df3c44e5ff3e9661a0ade3ca59cf
kw7oe <choongkwern@hotmail.com>
2021-06-12 20:43:51 +0800

    Add nested trace example according to Erlang opentelemtry.io docs

--- basic_elixir/lib/basic_elixir/worker.ex
+++ basic_elixir/lib/basic_elixir/worker.ex
@@ -1,14 +1,14 @@
 defmodule BasicElixir.Worker do
   use GenServer
   require Logger
-  require OpenTelemetry.Tracer
-  require OpenTelemetry.Span
+  require OpenTelemetry.Tracer, as: Tracer
+  require OpenTelemetry.Span, as: Span
 
   # Client
   def start_link(default) when is_list(default) do
-    OpenTelemetry.Tracer.with_span "start_link" do
-      span_ctx = OpenTelemetry.Tracer.current_span_ctx
-      OpenTelemetry.Span.set_attribute(span_ctx, "hello", "world")
+    Tracer.with_span "start_link" do
+      span_ctx = Tracer.current_span_ctx
+      Span.set_attribute(span_ctx, "hello", "world")
       GenServer.start_link(__MODULE__, default)
     end
   end
@@ -24,8 +24,9 @@ defmodule BasicElixir.Worker do
   # Server (callbacks)
   @impl true
   def init(stack) do
-    OpenTelemetry.Tracer.with_span "init" do
+    Tracer.with_span "init" do
       Logger.info("Starting #{__MODULE__}...")
+      BasicElixir.hello()
       {:ok, stack}
     end
   end
